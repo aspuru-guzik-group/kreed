c2aba68c702869c7a498f9b79c15ab01cdac6a70
alstonlo <40709307+alstonlo@users.noreply.github.com>
2022-10-14 19:58:16 -0400

    visualize

--- src/diffusion/schedules.py
+++ src/diffusion/schedules.py
@@ -7,14 +7,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def clip_noise_schedule(alphas_cumprod, clip_min=0.001):
+def clip_noise_schedule(alphas_cumprod, clip_margin=0.001):
     """For a noise schedule given by alpha^2, this clips alpha_t / alpha_t-1.
     This may help improve stability during sampling.
     """
 
     alphas_cumprod = np.concatenate([np.ones(1), alphas_cumprod], axis=0)
     alphas = alphas_cumprod[1:] / alphas_cumprod[:-1]
-    alphas = np.clip(alphas, a_min=clip_min, a_max=1.)
+    alphas = np.clip(alphas, a_min=clip_margin, a_max=(1 - clip_margin))
     return np.cumprod(alphas, axis=0)
 
 
@@ -26,7 +26,7 @@ def polynomial_schedule(timesteps, s=1e-5, power=2.0):
     t = np.linspace(0, T, T)
 
     f = (1 - np.power(t / T, power)) ** 2
-    f = clip_noise_schedule(f, clip_min=0.001)
+    f = clip_noise_schedule(f, clip_margin=0.001)
 
     alphas_cumprod = (1 - 2 * s) * f + s
     return alphas_cumprod
@@ -42,7 +42,7 @@ def cosine_beta_schedule(timesteps, s=0.008):
     f = np.cos(((t / T) + s) / (1 + s) * np.pi * 0.5) ** 2
     alphas_cumprod = f / f[0]
 
-    return clip_noise_schedule(alphas_cumprod, clip_min=0.001)
+    return clip_noise_schedule(alphas_cumprod, clip_margin=0.001)
 
 
 class PositiveLinear(nn.Module):
@@ -58,13 +58,13 @@ class PositiveLinear(nn.Module):
 
         self.weight = nn.Parameter(torch.empty((out_features, in_features)))
         if bias:
-            self.bias = torch.nn.Parameter(torch.empty(out_features))
+            self.bias = nn.Parameter(torch.empty(out_features))
         else:
             self.register_parameter("bias", None)
         self.reset_parameters()
 
     def reset_parameters(self) -> None:
-        torch.nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
 
         with torch.no_grad():
             self.weight.add_(self.weight_init_offset)
@@ -72,7 +72,7 @@ class PositiveLinear(nn.Module):
         if self.bias is not None:
             fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
             bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
-            torch.nn.init.uniform_(self.bias, -bound, bound)
+            nn.init.uniform_(self.bias, -bound, bound)
 
     def forward(self, inputs):
         positive_weight = F.softplus(self.weight)
@@ -85,56 +85,55 @@ class BaseNoiseSchedule(nn.Module):
     def forward(self, t):
         raise NotImplementedError()
 
-    def show_schedule(self, num_steps=50):
+    def sweep(self, num_steps=50):
         t = torch.linspace(0, 1, num_steps).view(num_steps, 1)
         gamma = self.forward(t)
         return t, gamma
 
 
-class PredefinedNoiseSchedule(BaseNoiseSchedule):
+class FixedNoiseSchedule(BaseNoiseSchedule):
     """Predefined noise schedule. Essentially creates a lookup array for predefined (non-learned) noise schedules.
     """
 
-    def __init__(self, noise_schedule, timesteps, s_poly=1e-5):
+    def __init__(self, shape, timesteps, s_poly=1e-5):
         super().__init__()
 
         self.timesteps = timesteps
 
-        if noise_schedule == "cosine":
+        if shape == "cosine":
             alphas_cumprod = cosine_beta_schedule(timesteps)
 
-        elif noise_schedule.startswith("polynomial"):
-            splits = noise_schedule.split("_")
+        elif shape.startswith("polynomial"):
+            splits = shape.split("_")
             assert len(splits) == 2
             power = float(splits[1])
             alphas_cumprod = polynomial_schedule(timesteps, s=s_poly, power=power)
 
         else:
-            raise ValueError(noise_schedule)
+            raise ValueError()
 
         alphas_cumprod = torch.from_numpy(alphas_cumprod).float()
-        gamma = torch.log(alphas_cumprod) - torch.log(1.0 - alphas_cumprod)
-        self.gamma = torch.nn.Parameter(gamma, requires_grad=False)
+        gamma = torch.log(1.0 - alphas_cumprod) - torch.log(alphas_cumprod)
+        self.gamma = nn.Parameter(gamma, requires_grad=False)
 
     def forward(self, t):
         t_int = torch.round(t * self.timesteps).long()
         return self.gamma[t_int]
 
 
-class GammaNetwork(BaseNoiseSchedule):
+class LearnedNoiseSchedule(BaseNoiseSchedule):
     """The gamma network models a monotonic increasing function. Construction as in the VDM paper.
     """
 
-    def __init__(self):
+    def __init__(self, d_hidden=1024):
         super().__init__()
 
         self.l1 = PositiveLinear(1, 1)
-        self.l2 = PositiveLinear(1, 1024)
-        self.l3 = PositiveLinear(1024, 1)
+        self.l2 = PositiveLinear(1, d_hidden)
+        self.l3 = PositiveLinear(d_hidden, 1)
 
-        self.gamma_0 = torch.nn.Parameter(torch.tensor([-5.0]))
-        self.gamma_1 = torch.nn.Parameter(torch.tensor([10.0]))
-        self.show_schedule()
+        self.gamma_0 = nn.Parameter(torch.tensor([-5.0]))
+        self.gamma_1 = nn.Parameter(torch.tensor([10.0]))
 
     def gamma_tilde(self, t):
         l1_t = self.l1(t)
---
+++ tests/visualize_schedules.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import torch
+import torch.nn.functional as F
+
+from src.diffusion.schedules import LearnedNoiseSchedule, FixedNoiseSchedule
+
+
+def visualize_schedules():
+    T = 1000
+
+    schedules = {
+        "polynomial_1": FixedNoiseSchedule("polynomial_1", timesteps=T, s_poly=1e-5),
+        "polynomial_2": FixedNoiseSchedule("polynomial_2", timesteps=T, s_poly=1e-5),
+        "cosine": FixedNoiseSchedule("cosine", timesteps=T),
+        "learned_init": LearnedNoiseSchedule(),
+        "learned_trained": LearnedNoiseSchedule(),
+    }
+
+    gt = schedules["polynomial_2"]
+    nn = schedules["learned_trained"]
+    optim = torch.optim.SGD(nn.parameters(), lr=1e-2)
+    for _ in range(2000):
+        nn.zero_grad()
+        loss = F.mse_loss(nn.sweep(T + 1)[1], gt.sweep(T + 1)[1])
+        loss.backward()
+        optim.step()
+
+    for label, schedule in schedules.items():
+        t, gammas = schedule.sweep(T + 1)
+        t = t.detach().cpu().numpy()
+        alphas_cumprod = torch.sigmoid(-gammas).detach().cpu().numpy()
+        plt.plot(t, alphas_cumprod, label=label)
+
+    plt.xlabel(r"$t$")
+    plt.ylabel(r"$\bar{\alpha}_t$")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    visualize_schedules()
