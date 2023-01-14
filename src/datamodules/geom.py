Runnable sign-diffusion need to check if rotation still works need to ensure that both are correct

src/datamodules/geom.py
@@ -92,15 +92,14 @@ class GEOMDataset(Dataset):
 
         abs_xyz[~abs_mask, :] = 0.0
 
-        G.ndata["abs_xyz"] = abs_xyz
-        G.ndata["abs_mask"] = abs_mask
-
-        G.ndata["abs_xyz_mask"] = (abs_xyz == 0.0)
-
         G.ndata['signs'] = torch.where(abs_xyz == 0.0, 0.0, G.ndata['xyz'] / abs_xyz)
 
         G.ndata['free_xyz'] = torch.where(G.ndata['signs'] == 0.0, G.ndata['xyz'], 0.0)
-        G.ndata['free_xyz_mask'] = ~G.ndata["abs_xyz_mask"]
+        G.ndata['free_mask'] = (abs_xyz == 0.0)
+
+        G.ndata["abs_xyz"] = abs_xyz
+        G.ndata["abs_mask"] = (abs_xyz != 0.0)
+        G.ndata["abs_node_mask"] = abs_mask
 
         if self.center_mean:
             # Center molecule coordinates to 0 CoM subspace

src/modules/classifiers.py
@@ -23,7 +23,7 @@ class KraitchmanClassifier(nn.Module):
         sum_logps = 0.0
 
         for G_true, G_pred in zip(dgl.unbatch(G_t), dgl.unbatch(G_rotated)):
-            cond_mask = G_true.ndata["abs_mask"]
+            cond_mask = G_true.ndata["abs_node_mask"]
 
             y_true = G_true.ndata["abs_xyz"]
             y_pred = G_pred.ndata["xyz"].abs()

src/modules/ddpm.py
@@ -346,21 +346,22 @@ class RefEquivariantDDPM(torch.nn.Module):
 
         G = G_init.local_var()
         if (mean is None) and (var is None) and (p is None):
-            G.ndata["free_xyz"] = torch.where(G.ndata['free_mask'], eps, 0.0)
+            G.ndata["free_xyz"] = torch.where(G.ndata['free_mask'], eps, 0.0) # zero out abs nodes
+
+            sign_flips_01 = torch.bernoulli(0.5 * torch.ones_like(G.ndata['signs']))
         else:
             assert (mean is not None) and (var is not None) and (p is not None)
             std = dgl.broadcast_nodes(G, var.sqrt())
             std = torch.broadcast_to(std.unsqueeze(-1), mean.shape)
             G.ndata["free_xyz"] = torch.where(G.ndata['free_mask'], mean + std * eps, 0.0) # zero out abs nodes
             
-            new_signs_01 = torch.bernoulli(p)
+            sign_flips_01 = torch.bernoulli(p)
 
-            # signs: 0 | 1  -->  -1 | +1
-            new_signs_p1m1 = 2*new_signs_01 - 1.0
-            flipped_signs = G.ndata['signs'] * new_signs_p1m1
-            G.ndata['signs'] = torch.where(G.ndata['abs_mask'], new_signs_p1m1, 0.0) # zero out free nodes
-            G.ndata['xyz'] = G.ndata['signs'] * G.ndata['abs_xyz'] + G.ndata['free_xyz']
-        return G if not return_noise else (G, eps, flipped_signs)
+        # signs: 0 | 1  -->  -1 | +1
+        sign_flips_p1m1 = 2*sign_flips_01 - 1.0
+        G.ndata['signs'] = torch.where(G.ndata['abs_mask'], G.ndata['signs'] * sign_flips_p1m1, 0.0) # zero out free nodes
+        G.ndata['xyz'] = G.ndata['signs'] * G.ndata['abs_xyz'] + G.ndata['free_xyz']
+        return G if not return_noise else (G, eps, sign_flips_p1m1)
 
     def sample_q_Gt_given_Gs(self, G_s, s, t, return_noise=False):
         """Samples from G_t ~ q(G_t|G_s), where s < t."""
@@ -418,7 +419,7 @@ class RefEquivariantDDPM(torch.nn.Module):
         else:
             return G_t, frames
 
-    def noise_error(self, G, pred_eps, true_eps, reduction):
+    def noise_error(self, G, pred_eps, true_eps, true_sign_flips, reduction):
         with G.local_scope():
             G.ndata["error"] = torch.where(G.ndata['free_mask'], (pred_eps - true_eps).square(), 0.0)
             if reduction == "sum":
@@ -431,10 +432,10 @@ class RefEquivariantDDPM(torch.nn.Module):
         abs_mask = G.ndata['abs_mask']
         pred_sign_probs = torch.sigmoid(pred_eps[abs_mask])
 
-        # for now, try to predict G_0's signs, rather than predicting which signs were flipped
-        true_sign_targets_p1m1 = G.ndata['signs'][abs_mask]
+        # try to predict which signs were flipped
+        true_sign_targets_p1m1 = true_sign_flips
         true_sign_targets_01 = (true_sign_targets_p1m1 + 1) / 2
-        sign_bce = F.binary_cross_entropy(pred_sign_probs, true_sign_targets_01, reduction=reduction)
+        sign_bce = F.binary_cross_entropy(pred_sign_probs, true_sign_targets_01[abs_mask], reduction=reduction)
 
         return error*self.loss_weight + sign_bce
 
@@ -484,7 +485,7 @@ class RefEquivariantDDPM(torch.nn.Module):
         SNR_weight = torch.where(t_int == 1, 1, SNR_weight).squeeze(-1)  # w(0) = 1
 
         # sample G_t
-        G_t, eps, flipped_signs = self.sample_q_Gt_given_Gs(G_s=G_0, s=0, t=t, return_noise=True)
+        G_t, eps, sign_flips = self.sample_q_Gt_given_Gs(G_s=G_0, s=0, t=t, return_noise=True)
 
         # Neural net prediction.
         pred_eps = self.dynamics(G=G_t, t=t)
@@ -497,7 +498,7 @@ class RefEquivariantDDPM(torch.nn.Module):
 
             use_l2 = (self.loss_type == "L2")
 
-            error = self.noise_error(G_t, pred_eps=pred_eps, true_eps=eps, reduction="mean")
+            error = self.noise_error(G_t, pred_eps=pred_eps, true_eps=eps, true_sign_flips=sign_flips, reduction="mean")
             loss_tm1 = 0.5 * (1.0 if use_l2 else SNR_weight) * error
 
             if use_l2:
@@ -508,13 +509,13 @@ class RefEquivariantDDPM(torch.nn.Module):
 
         else:
 
-            error = self.noise_error(G_t, pred_eps=pred_eps, true_eps=eps, reduction="sum")
+            error = self.noise_error(G_t, pred_eps=pred_eps, true_eps=eps, true_sign_flips=sign_flips, reduction="sum")
             loss_tm1 = 0.5 * SNR_weight * error
 
             t_1 = torch.ones_like(t) / self.T
-            G_1, eps, flipped_signs1 = self.sample_q_Gt_given_Gs(G_s=G_0, s=0, t=t_1, return_noise=True)
+            G_1, eps, sign_flips1 = self.sample_q_Gt_given_Gs(G_s=G_0, s=0, t=t_1, return_noise=True)
             pred_eps = self.dynamics(G=G_1, t=t_1)
-            error = self.noise_error(G_t, pred_eps=pred_eps, true_eps=eps, reduction="sum")
+            error = self.noise_error(G_t, pred_eps=pred_eps, true_eps=eps, true_sign_flips=sign_flips1, reduction="sum")
 
             loss_0 = (0.5 * error) - self.log_norm_const_p_G0_given_G1(G_0=G_0)
             loss = loss_prior + ((self.T - 1) * loss_tm1) + loss_0

src/modules/egnn.py
@@ -16,13 +16,19 @@ from src.kraitchman import ATOM_MASSES
 
 class EGNNConv(nn.Module):
 
-    def __init__(self, in_size, hidden_size, out_size, edge_feat_size=0):
+    def __init__(self, in_size, hidden_size, out_size, edge_feat_size=0, equivariance='rotation'):
         super().__init__()
 
         self.in_size = in_size
         self.hidden_size = hidden_size
         self.out_size = out_size
         self.edge_feat_size = edge_feat_size
+        self.equivariance = equivariance
+
+        if self.equivariance == 'rotation':
+            self.norm = lambda x: x.square()
+        elif self.equivariance == 'reflection':
+            self.norm = lambda x: x.abs()
 
         act_fn = nn.SiLU()
 
@@ -91,7 +97,7 @@ class EGNNConv(nn.Module):
 
             # get coordinate diff & radial features
             graph.apply_edges(fn.u_sub_v("x", "x", "x_diff"))
-            graph.edata["radial"] = graph.edata["x_diff"].square().sum(dim=1).unsqueeze(-1)
+            graph.edata["radial"] = self.norm(graph.edata["x_diff"]).sum(dim=1).unsqueeze(-1)
 
             # normalize coordinate difference
             graph.edata["x_diff"] = graph.edata["x_diff"] / (graph.edata["radial"].sqrt() + 1.0)
@@ -118,18 +124,28 @@ class EGNNDynamics(nn.Module):
         d_atom_vocab,
         d_hidden,
         n_layers,
+        equivariance,
     ):
         super().__init__()
 
         self.d_atom_vocab = d_atom_vocab
         self.lin_hid = nn.Linear(6 + d_atom_vocab, d_hidden)
+        self.equivariance = equivariance
+
+        if self.equivariance == 'rotation':
+            self.norm = lambda x: x.square()
+            self.maybe_center = lambda xyz, G: dists.centered_mean(G, xyz - G.ndata["xyz"])
+        elif self.equivariance == 'reflection':
+            self.norm = lambda x: x.abs()
+            self.maybe_center = lambda xyz, G: xyz
 
         self.egnn_layers = nn.ModuleList([
             EGNNConv(
                 in_size=d_hidden,
                 hidden_size=d_hidden,
                 out_size=(0 if (i + 1 == n_layers) else d_hidden),
-                edge_feat_size=1
+                edge_feat_size=1,
+                equivariance=self.equivariance,
             )
             for i in range(n_layers)
         ])
@@ -143,7 +159,7 @@ class EGNNDynamics(nn.Module):
         temb = dgl.broadcast_nodes(G, t).to(xyz)  # (N 1)
 
         # Conditioning features
-        cond_mask = G.ndata["abs_mask"].to(xyz)  # (N)
+        cond_mask = G.ndata["abs_node_mask"].to(xyz)  # (N)
         abs_xyz = G.ndata["abs_xyz"].to(xyz)  # (N 3)
 
         features = [
@@ -162,70 +178,9 @@ class EGNNDynamics(nn.Module):
 
         with G.local_scope():
             G.apply_edges(fn.u_sub_v("xyz", "xyz", "xyz_diff"))
-            a = G.edata["xyz_diff"].square().sum(dim=1).unsqueeze(-1)
+            a = self.norm(G.edata["xyz_diff"]).sum(dim=1).unsqueeze(-1)
 
         h = self.lin_hid(h)
         for layer in self.egnn_layers:
             h, xyz = layer(G, node_feat=h, coord_feat=xyz, edge_feat=a)
-        vel = xyz - G.ndata["xyz"]
-        return dists.centered_mean(G, vel)
-
-class RefEGNNDynamics(nn.Module):
-
-    def __init__(
-        self,
-        d_atom_vocab,
-        d_hidden,
-        n_layers,
-    ):
-        super().__init__()
-
-        self.d_atom_vocab = d_atom_vocab
-        
-        # atom_vocab + atom_mass + t + abs_mask + abs_xyz
-        self.lin_hid = nn.Linear(d_atom_vocab + 1 + 1 + 3 + 3, d_hidden)
-
-        self.egnn_layers = nn.ModuleList([
-            EGNNConv(
-                in_size=d_hidden,
-                hidden_size=d_hidden,
-                out_size=(0 if (i + 1 == n_layers) else d_hidden),
-                edge_feat_size=1
-            )
-            for i in range(n_layers)
-        ])
-
-    def _featurize_nodes(self, G, t):
-        xyz = G.ndata["xyz"]  # for casting
-
-        # Node features
-        atom_ids = F.one_hot(G.ndata["atom_ids"], num_classes=self.d_atom_vocab).to(xyz)  # (N d_vocab)
-        atom_masses = ATOM_MASSES[G.ndata["atom_nums"].cpu()].to(xyz)  # (N)
-        temb = dgl.broadcast_nodes(G, t).to(xyz)  # (N 1)
-
-        # Conditioning features
-        cond_mask = G.ndata["abs_mask"].to(xyz)  # (N 3)
-        abs_xyz = G.ndata["abs_xyz"].to(xyz)  # (N 3)
-
-        features = [
-            atom_ids,
-            atom_masses.unsqueeze(-1) / 12.0,  # FIXME: the normalization is arbitrary here
-            temb,
-            cond_mask,
-            abs_xyz,
-        ]
-
-        return torch.cat(features, dim=-1)  # (N d_vocab+1+1+3+3)
-
-    def forward(self, G, t):
-        xyz = G.ndata["xyz"]
-        h = self._featurize_nodes(G=G, t=t)
-
-        with G.local_scope():
-            G.apply_edges(fn.u_sub_v("xyz", "xyz", "xyz_diff"))
-            a = G.edata["xyz_diff"].abs().sum(dim=1).unsqueeze(-1) # replace with L1
-
-        h = self.lin_hid(h)
-        for layer in self.egnn_layers:
-            h, xyz = layer(G, node_feat=h, coord_feat=xyz, edge_feat=a)
-        return xyz
+        return self.maybe_center(xyz, G)