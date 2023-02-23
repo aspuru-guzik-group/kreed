1339a7791a3578171c9acad6f471795417464e90
Austin Cheng <auhcheng@gmail.com>
2023-02-23 10:32:11 -0500

    Pick one good flip

--- src/evaluate.py
+++ src/evaluate.py
@@ -19,41 +19,37 @@ def get_greedy_mapping(xyz_pred, xyz_truth, atom_nums):
 
     return mapping
 
-flips = [
-    torch.tensor([-1, -1, -1]),
-    torch.tensor([-1, -1, 1]),
-    torch.tensor([-1, 1, -1]),
-    torch.tensor([-1, 1, 1]),
-    torch.tensor([1, -1, -1]),
-    torch.tensor([1, -1, 1]),
-    torch.tensor([1, 1, -1]),
-    torch.tensor([1, 1, 1]),
-]
+def get_flip(G_pred, G_truth):
+    mask = torch.logical_and(G_pred.ndata['cond_mask'], torch.abs(G_pred.ndata['xyz']) > .5)
+    mask = torch.all(mask, dim=1)
+    nonzero = torch.nonzero(mask)
+    if nonzero.shape[0] > 0:
+        # there are no labeled atoms far from origin
+        carbon_idx = nonzero[0][0]
+    else:
+        # pick any labeled atom
+        carbon_idx = torch.all(G_pred.ndata['cond_mask'], dim=1).nonzero()[0][0]
+
+    flip = G_pred.ndata['xyz'][carbon_idx] / torch.abs(G_truth.ndata['xyz'][carbon_idx])
+    flip = flip > 0
+    flip = flip * 2 - 1
+
+    return flip
 
 def align_and_rmsd(G_pred, G_truth):
-    # min_rmsd = 100000
-    min_heavy_rmsd = 100000
-    best_flip = None
-    for flip in flips:
-        xyz_pred = G_pred.ndata['xyz'] * flip
-        xyz_truth = G_truth.ndata['xyz']
-        atom_nums = G_pred.ndata['atom_nums']
+    flip = get_flip(G_pred, G_truth)
+    xyz_pred = G_pred.ndata['xyz'] * flip
+    xyz_truth = G_truth.ndata['xyz']
+    atom_nums = G_pred.ndata['atom_nums']
 
-        # mapping = get_greedy_mapping(xyz_pred, xyz_truth, atom_nums)
-        # rmsd = torch.norm(xyz_pred - xyz_truth[mapping]).mean().sqrt()
-        # min_rmsd = min(min_rmsd, rmsd)
+    # heavy atoms only
+    xyz_pred = xyz_pred[atom_nums != 1]
+    xyz_truth = xyz_truth[atom_nums != 1]
+    atom_nums = atom_nums[atom_nums != 1]
+    mapping = get_greedy_mapping(xyz_pred, xyz_truth, atom_nums)
+    heavy_rmsd = torch.norm(xyz_pred - xyz_truth[mapping]).mean().sqrt()
 
-        # heavy atoms only
-        xyz_pred = xyz_pred[atom_nums != 1]
-        xyz_truth = xyz_truth[atom_nums != 1]
-        atom_nums = atom_nums[atom_nums != 1]
-        mapping = get_greedy_mapping(xyz_pred, xyz_truth, atom_nums)
-        heavy_rmsd = torch.norm(xyz_pred - xyz_truth[mapping]).mean().sqrt()
-        if heavy_rmsd < min_heavy_rmsd:
-            min_heavy_rmsd = heavy_rmsd
-            best_flip = flip
-
-    return min_heavy_rmsd, best_flip
+    return heavy_rmsd, flip
 
 def evaluate(G_pred, G_truth):
     coords_pred = G_pred.ndata['xyz']
