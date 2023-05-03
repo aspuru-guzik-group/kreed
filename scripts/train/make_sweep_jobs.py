58ad0ec58fb11d5e46a69ac6b34a50899cd9a217
alstonlo <40709307+alstonlo@users.noreply.github.com>
2023-05-03 19:12:49 -0400

    update sweep

--- scripts/train/make_sweep_jobs.py
+++ scripts/train/make_sweep_jobs.py
@@ -33,8 +33,9 @@ eval ${{lines[SLURM_ARRAY_TASK_ID]}} --wandb_run_id ${{SLURM_JOB_ID}} --checkpoi
 TRAIN_COMMAND_TEMPLATE = (
     "srun python -m src.experimental.train "
     "--accelerator=gpu --devices=1 --num_workers=8 "
-    "--dataset=qm9 --enable_wandb "
-    "--check_samples_every_n_epochs=50 --samples_assess_n_batches=10 --samples_visualize_n_mols=0"
+    "--dataset=qm9 --enable_wandb --wandb_project=sweep_train_edm_qm9 "
+    "--epochs=3000 "
+    "--check_samples_every_n_epochs=100 --samples_assess_n_batches=10 --samples_visualize_n_mols=0"
 )
 
 SWEEP_GRID = [
--- scripts/train/train_edm_qm9.sh
+++ scripts/train/train_edm_qm9.sh
@@ -21,8 +21,4 @@ conda activate edm
 
 cd ../..
 
-srun python -m src.experimental.train \
-  --accelerator=gpu --devices=1 --num_workers=8 \
-  --dataset=qm9 \
-  --enable_wandb --wandb_run_id ${SLURM_JOB_ID} --checkpoint_dir /checkpoint/${USER}/${SLURM_JOB_ID} \
-  --check_samples_every_n_epochs=50
+srun python -m src.experimental.train --accelerator=gpu --devices=1 --num_workers=8 --dataset=qm9 --enable_wandb --wandb_project=sweep_train_edm_qm9_debug --epochs=10 --check_samples_every_n_epochs=1 --samples_assess_n_batches=1 --samples_visualize_n_mols=0 --parameterization=eps --egnn_equivariance=e3   --wandb_run_name=eps-e3-ortho
