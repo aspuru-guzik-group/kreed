fb3e63281f6524181bd0a95e42dea3ed45b9d70e
alstonlo <40709307+alstonlo@users.noreply.github.com>
2023-06-07 18:30:09 -0400

    ddp geom training script

--- scripts/train/train_geom_ddp.sh
+++ scripts/train/train_geom_ddp.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 #SBATCH --job-name=train_edm
-#SBATCH --ntasks=1
-#SBATCH --gpus-per-task=1
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:4
+#SBATCH --tasks-per-node=4
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=32GB
+#SBATCH --mem=0
 #SBATCH --partition=rtx6000
 #SBATCH --qos=normal
 #SBATCH --export=ALL
 #SBATCH --output=logs/slurm-%j.out
+
+export NCCL_SOCKET_IFNAME=bond0
 
 # Conda
 source ~/.bashrc
@@ -16,8 +19,8 @@ conda activate egnn
 
 cd ../..
 
-srun python -m src.experimental.train \
-  --accelerator=gpu --devices=1 \
+srun --nodes=2 --tasks-per-node=4  python -m src.experimental.train \
+  --accelerator=gpu --nodes=2  --devices=4 \
   --batch_size=16 --num_workers=8 --dataset=geom \
   --max_epochs=100 --check_samples_every_n_epochs=5 --samples_assess_n_batches=10 --samples_visualize_n_mols=0 \
   --hidden_features=512 --inner_features=512 \
