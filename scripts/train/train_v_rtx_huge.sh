42febd23114e65644a5755e08c24be3f1d1aeda0
Austin Cheng <auhcheng@gmail.com>
2023-06-16 15:47:40 -0400

    fix copy to scratch

--- scripts/train/train_v_rtx_huge.sh
+++ scripts/train/train_v_rtx_huge.sh
@@ -20,5 +20,5 @@ conda activate egnn
 
 srun --nodes=3 --tasks-per-node=4 python -m src.experimental.train --num_nodes=3 --accelerator=gpu --devices=4 --num_workers=10 --dataset=geom --enable_wandb --wandb_project=transformer_geom --max_epochs=100 --check_samples_every_n_epochs=5 --batch_size=64 --wandb_run_name=v_transformer_rtx_huge --lr=2.5e-4 --checkpoint_dir=/checkpoint/${USER}/${SLURM_JOB_ID} --strategy=ddp --wandb_run_id=${SLURM_JOB_ID}
 
-cp /checkpoint/${USER}/${SLURM_JOB_ID} /scratch/${USER}/${SLURM_JOB_ID} -r
+cp /checkpoint/${USER}/${SLURM_JOB_ID} ${SCRATCH} -r
 
