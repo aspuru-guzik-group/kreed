19746a10c2e7ea25a0c8c933609ce5a2515f4746
alstonlo <40709307+alstonlo@users.noreply.github.com>
2023-06-26 21:13:30 -0400

    forgot to set pdropout_cond in GEOM training

--- scripts/train/train_geom_ddp.sh
+++ scripts/train/train_geom_ddp.sh
@@ -21,7 +21,7 @@ cd ../..
 
 srun --nodes=2 --tasks-per-node=4  python -m src.experimental.train \
   --accelerator=gpu --num_nodes=2  --devices=4 \
-  --batch_size=64 --num_workers=8 --dataset=geom \
+  --batch_size=64 --num_workers=8 --dataset=geom --pdropout_cond 0.0 1.0 \
   --max_epochs=100 --check_samples_every_n_epochs=5 --samples_assess_n_batches=1 --samples_visualize_n_mols=0 \
   --lr=0.0004 \
   --enable_wandb  --wandb_project=train_geom_uniform --wandb_run_id ${SLURM_JOB_ID} --checkpoint_dir /checkpoint/${USER}/${SLURM_JOB_ID}
